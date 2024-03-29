9688d8269a75dd4e8a3404487f6ffa69ac58022c
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-03-29 09:10:22 +0700

    void input

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -5,3 +5,23 @@ int arr[20];
 int cmp_count = 0;
 int mov_count = 0;
 int n;
+
+void input() {
+	while (true) {
+		cout << "Masukkan panjang elemen array = ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+	cout << "\n================" << endl;
+	cout << "\nEnter array element" << endl;
+	cout << "\n================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[1];
+	}
+}
